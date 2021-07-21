bdfb273752b1a7603c5a91850205e7a6d2957c2b
VIkash <itsmevikash09@gmail.com>
2021-07-21 16:26:34 +0530

    ..

---
+++ app/src/main/java/com/example/pdcast/ui/view/EpisodeItemAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.pdcast.ui.view
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pdcast.data.response.RssFeedResponse
+import com.example.pdcast.databinding.EpisodeItemsBinding
+
+class EpisodeItemAdapter(private val episodes: List<RssFeedResponse.EpisodeResponse>,private val listener:(String) -> Unit) : RecyclerView.Adapter<EpisodeItemAdapter.EpisodeItemViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EpisodeItemViewHolder {
+        val binding = EpisodeItemsBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+       return  EpisodeItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: EpisodeItemViewHolder, position: Int) {
+        val currentItem = episodes[position]
+        holder.bind(currentItem)
+    }
+
+    override fun getItemCount(): Int {
+        return episodes.size
+    }
+
+
+    inner class EpisodeItemViewHolder(private val binding: EpisodeItemsBinding) :RecyclerView.ViewHolder(binding.root) {
+        fun bind(currentItem: RssFeedResponse.EpisodeResponse) {
+            binding.tvTitle.text = currentItem.title
+            binding.tvDescription.text= currentItem.description
+            binding.tvPubdate.text = currentItem.pubDate
+            binding.duration.text = currentItem.duration
+            binding.play.setOnClickListener {
+                Log.d(TAG, "bind-Link: ${currentItem.episodeUrl}")
+                currentItem.episodeUrl?.let { it1 -> listener(it1) }
+            }
+        }
+
+    }
+
+    companion object{
+        private const val TAG = "EpisodeItemAdapter"
+    }
+}
