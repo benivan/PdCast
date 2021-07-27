86a6f830d3fb2de254730472fe98f3c90e3e7ad1
VIkash <itsmevikash09@gmail.com>
2021-07-28 00:21:42 +0530

    repo

---
+++ app/src/main/java/com/example/pdcast/data/repository/PodcastRepository.kt
@@ -0,0 +1,34 @@
+package com.example.pdcast.data.repository
+
+import android.util.Log
+import com.example.pdcast.data.api.ItunesService
+import com.example.pdcast.data.api.RssFeedService
+import com.example.pdcast.data.model.ItunesPodcast
+import com.example.pdcast.data.model.PodcastModel
+import com.example.pdcast.data.response.PodcastResponse
+
+class PodcastRepository {
+
+    private val podcastService = ItunesService.createItunesService()
+
+
+
+
+    suspend fun getPodcastWithTerms(
+        term: String
+    ): PodcastModel {
+        val searchPodcastByTerms = podcastService.searchPodcastByTerms(term)
+        val toList = searchPodcastByTerms.results.map {
+            ItunesPodcast(
+                collectionCensoredName = it.collectionCensoredName,
+                feedUrl = it.feedUrl ?: "" ,
+                artworkUrl30 = it.artworkUrl30,
+                releaseDate = it.releaseDate
+            )
+        }.toList()
+        return PodcastModel(searchPodcastByTerms.resultCount, toList)
+    }
+    companion object{
+        private const val TAG = "PodcastRepository"
+    }
+}
