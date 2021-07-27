3478b843ebf82fbceffc38b7fdb46a7daa853710
VIkash <itsmevikash09@gmail.com>
2021-07-28 00:25:09 +0530

    fetch xml feed from url

---
+++ app/src/main/java/com/example/pdcast/data/api/FeedService.kt
@@ -0,0 +1,76 @@
+package com.example.pdcast.data.api
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.suspendCancellableCoroutine
+import okhttp3.*
+import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+
+interface ApiResponse<T> {
+    fun onSuccess(data: T)
+    fun onFailure(throwable: Throwable)
+}
+
+object RssFeedService {
+
+    fun getFeed(xmlFileURL: String, callBack: ApiResponse<String>) {
+
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url(xmlFileURL)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                callBack.onFailure(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        callBack.onSuccess(it.string())
+                    }
+                }
+                callBack.onFailure(Throwable("Response not successful"))
+            }
+
+        })
+
+
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    suspend fun getFeedXml(xmlFileURL: String):
+            String = suspendCancellableCoroutine {
+
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url(xmlFileURL)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                it.resumeWithException(exception = Throwable(e))
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                     response.body()?.let { body ->  it.resume(body.string())}
+                }
+            }
+        })
+    }
+
+
+}
+
+
+
+
+
+
