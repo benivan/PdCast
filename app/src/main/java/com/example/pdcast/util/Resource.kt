5665b66a4b00b61fc93ea1aab3e9be9ab842793d
VIkash <itsmevikash09@gmail.com>
2021-09-21 13:07:26 +0530

    Resource response

---
+++ app/src/main/java/com/example/pdcast/util/Resource.kt
@@ -0,0 +1,9 @@
+package com.example.pdcast.util
+
+sealed class Resource<out T> {
+    class None<out T> : Resource<T>()
+    class Loading<out T> : Resource<T>()
+    data class Success<out T>(val data: T) : Resource<T>()
+    data class Failure<out T>(val throwable: Throwable) : Resource<T>()
+}
+
