0cbcef2981f28c7142c4fbc9bd397c75dd64f30b
VIkash <itsmevikash09@gmail.com>
2021-09-22 15:16:37 +0530

    podcast repo

--- app/src/main/java/com/example/pdcast/data/repository/PodcastRepository.kt
+++ app/src/main/java/com/example/pdcast/data/repository/PodcastRepository.kt
@@ -1,34 +1,15 @@
 package com.example.pdcast.data.repository
 
-import android.util.Log
-import com.example.pdcast.data.api.ItunesService
-import com.example.pdcast.data.api.RssFeedService
-import com.example.pdcast.data.model.ItunesPodcast
-import com.example.pdcast.data.model.PodcastModel
-import com.example.pdcast.data.response.PodcastResponse
+import androidx.lifecycle.LiveData
+import com.example.pdcast.data.dao.PodcastSubscribeDao
+import com.example.pdcast.data.model.PodcastDataBaseModel
 
-class PodcastRepository {
+class PodcastRepository(private val podcastSubscribeDao: PodcastSubscribeDao) {
 
-    private val podcastService = ItunesService.createItunesService()
+    val readAllData: LiveData<List<PodcastDataBaseModel>> =
+        podcastSubscribeDao.readAllSubscribedPodcasts()
 
-
-
-
-    suspend fun getPodcastWithTerms(
-        term: String
-    ): PodcastModel {
-        val searchPodcastByTerms = podcastService.searchPodcastByTerms(term)
-        val toList = searchPodcastByTerms.results.map {
-            ItunesPodcast(
-                collectionCensoredName = it.collectionCensoredName,
-                feedUrl = it.feedUrl ?: "" ,
-                artworkUrl30 = it.artworkUrl30,
-                releaseDate = it.releaseDate
-            )
-        }.toList()
-        return PodcastModel(searchPodcastByTerms.resultCount, toList)
-    }
-    companion object{
-        private const val TAG = "PodcastRepository"
+    suspend fun addPodcast(podcastDataBaseModel: PodcastDataBaseModel) {
+        podcastSubscribeDao.insertPodcast(podcastDataBaseModel)
     }
 }
