eb42a5d46b576de0cf3d92b25511cf990bc5ee1a
VIkash <itsmevikash09@gmail.com>
2021-09-22 15:15:19 +0530

    podcast model

---
+++ app/src/main/java/com/example/pdcast/data/model/PodcastModel.kt
@@ -0,0 +1,18 @@
+package com.example.pdcast.data.model
+
+import androidx.room.AutoMigration
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+data class PodcastModel(
+    val resultCount: Int,
+    val results: List<ItunesPodcast>
+)
+
+data class ItunesPodcast(
+    val collectionCensoredName: String,
+    val feedUrl: String,
+    val artworkUrl30: String,
+    val artworkUrl100: String,
+    val releaseDate: String
+)
