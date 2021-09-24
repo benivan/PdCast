8688cdb7fcc25f3613c843770478c18d761356d7
VIkash <itsmevikash09@gmail.com>
2021-09-24 18:27:00 +0530

    cancel scope

--- app/src/main/java/com/example/pdcast/ui/PodcastViewModel.kt
+++ app/src/main/java/com/example/pdcast/ui/PodcastViewModel.kt
@@ -1,17 +1,31 @@
 package com.example.pdcast.ui
 
+import android.content.Context
+import android.media.MediaMetadata
+import android.media.MediaMetadata.METADATA_KEY_ALBUM_ART_URI
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings.Secure.getString
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI
+import android.support.v4.media.session.MediaControllerCompat
 import android.util.Log
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.pdcast.R
 import com.example.pdcast.data.api.RssFeedService
 import com.example.pdcast.data.model.PodcastModel
 import com.example.pdcast.data.response.RssFeedResponse
+import com.example.pdcast.ui.view.PodcastDetailAndEpisodeFragment
 import com.example.pdcast.util.Resource
 import com.example.pdcast.util.RssXmlParser
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import java.lang.Exception
+import java.time.Duration
 
 class PodcastViewModel : ViewModel() {
 
@@ -20,10 +34,11 @@ class PodcastViewModel : ViewModel() {
 
     val podcast: SharedFlow<Resource<RssFeedResponse>> = _podcast
 
+
+
     private val rssXmlParser = RssXmlParser()
 
     private var isFirstTime: Boolean = true
-
 
 
 
@@ -52,7 +67,19 @@ class PodcastViewModel : ViewModel() {
     }
 
 
+
+
+
+
+    override fun onCleared() {
+        viewModelScope.cancel()
+    }
+
+
     companion object {
         private const val TAG = "PodcastViewModel"
     }
 }
+
+
+
