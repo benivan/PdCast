5916351c67476cb233ba9caefc1bc76d2183ea61
VIkash <itsmevikash09@gmail.com>
2021-10-06 14:39:44 +0530

    added function to MainViewModel

--- app/src/main/java/com/example/pdcast/ui/MainViewModel.kt
+++ app/src/main/java/com/example/pdcast/ui/MainViewModel.kt
@@ -1,14 +1,31 @@
 package com.example.pdcast.ui
 
+import android.app.Application
 import android.support.v4.media.session.MediaControllerCompat
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.pdcast.data.database.PodcastSubscribedDatabase
+import com.example.pdcast.data.model.PodcastDataBaseModel
+import com.example.pdcast.data.repository.PodcastRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
-class PlayerVIewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    val readAllData: LiveData<List<PodcastDataBaseModel>>
+    private val podcastRepository: PodcastRepository
+
+    init {
+        val podcastSubscribeDao =
+            PodcastSubscribedDatabase.getDatabase(application).podcastSubscribeDao()
+        podcastRepository = PodcastRepository(podcastSubscribeDao)
+        readAllData = podcastRepository.readAllData
+    }
 
     private var _controller: MediaControllerCompat? = null
 
@@ -31,6 +48,12 @@ class PlayerVIewModel : ViewModel() {
 
     fun startPlayFromUri(isPlayFromUri:Boolean) = viewModelScope.launch{
         _playFromUri.emit(isPlayFromUri)
+    }
+
+    fun addPodcast(podcast: PodcastDataBaseModel) {
+        viewModelScope.launch(Dispatchers.IO) {
+            podcastRepository.addPodcast(podcast)
+        }
     }
 
 
