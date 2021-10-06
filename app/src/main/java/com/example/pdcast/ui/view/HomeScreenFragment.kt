c567503999fa96a255ecf07cba2494fab9c30523
VIkash <itsmevikash09@gmail.com>
2021-10-06 14:36:59 +0530

    MainPodcastViewModel to MainViewModel

--- app/src/main/java/com/example/pdcast/ui/view/HomeScreenFragment.kt
+++ app/src/main/java/com/example/pdcast/ui/view/HomeScreenFragment.kt
@@ -11,7 +11,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.pdcast.databinding.FragmentHomeScreenBinding
-import com.example.pdcast.ui.MainPodcastViewModel
+import com.example.pdcast.ui.MainViewModel
 import com.example.pdcast.util.DataMapper
 import com.google.android.material.transition.MaterialFadeThrough
 import kotlinx.coroutines.flow.onEach
@@ -22,7 +22,7 @@ class HomeScreenFragment : Fragment() {
     private var _binding: FragmentHomeScreenBinding? = null
     private val binding: FragmentHomeScreenBinding get() = _binding!!
 
-    private val viewModel: MainPodcastViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels()
 
 
     override fun onCreateView(
