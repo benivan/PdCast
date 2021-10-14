325bf1786af6512d7ef95944df53e50880bec444
VIkash <itsmevikash09@gmail.com>
2021-10-15 01:45:15 +0530

    class changed

--- app/src/main/java/com/example/pdcast/data/dao/PodcastSubscribeDao.kt
+++ app/src/main/java/com/example/pdcast/data/dao/PodcastSubscribeDao.kt
@@ -5,16 +5,15 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.pdcast.data.model.ItunesPodcast
-import com.example.pdcast.data.model.PodcastDataBaseModel
+import com.example.pdcast.data.dto.DBPodcast
 
 @Dao
 interface PodcastSubscribeDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPodcast(podcast: PodcastDataBaseModel)
+    suspend fun insertPodcast(DBPodcast: DBPodcast)
 
     @Query("SELECT * FROM podcast_subscribe_table ORDER BY id ASC")
-    fun readAllSubscribedPodcasts(): LiveData<List<PodcastDataBaseModel>>
+    fun readAllSubscribedPodcasts(): LiveData<List<DBPodcast>>
 
 }
