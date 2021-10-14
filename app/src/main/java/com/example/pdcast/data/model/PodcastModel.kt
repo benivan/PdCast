d0a82e1ee35946b7a2118330c42d667ea192d638
VIkash <itsmevikash09@gmail.com>
2021-10-15 01:43:49 +0530

    podcast model

---
+++ app/src/main/java/com/example/pdcast/data/model/PodcastModel.kt
@@ -0,0 +1,10 @@
+package com.example.pdcast.data.model
+
+data class PodcastModel(
+    val podcastId:Long?,
+    val title:String,
+    val description:String,
+    val language:String,
+    val link:String,
+    val imageUrl:String
+)
