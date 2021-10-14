ee7ee562e6195a52b63b47411a2d2f74f60e403f
VIkash <itsmevikash09@gmail.com>
2021-10-15 01:42:03 +0530

    database attached

--- app/src/main/java/com/example/pdcast/ui/view/HomeScreenFragment.kt
+++ app/src/main/java/com/example/pdcast/ui/view/HomeScreenFragment.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.VerifiedInputEvent
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.pdcast.data.model.PodcastModel
+import com.example.pdcast.data.response.RssFeedResponse
 import com.example.pdcast.databinding.FragmentHomeScreenBinding
 import com.example.pdcast.ui.MainViewModel
 import com.example.pdcast.util.DataMapper
 import com.google.android.material.transition.MaterialFadeThrough
-import kotlinx.coroutines.flow.onEach
 
 
 class HomeScreenFragment : Fragment() {
@@ -22,7 +24,8 @@ class HomeScreenFragment : Fragment() {
     private var _binding: FragmentHomeScreenBinding? = null
     private val binding: FragmentHomeScreenBinding get() = _binding!!
 
-    private val viewModel: MainViewModel by viewModels()
+    private val mainViewModel: MainViewModel by activityViewModels()
+
 
 
     override fun onCreateView(
@@ -43,19 +46,70 @@ class HomeScreenFragment : Fragment() {
 
         val dataMapper = DataMapper()
 
-       viewModel.readAllData.observe(viewLifecycleOwner, Observer { listOfPodcastDatabaseModel ->
-           binding.errorMessage.visibility =View.GONE
-           binding.homeScreenProgressBar.visibility =View.GONE
-           val listOfPodcasts = listOfPodcastDatabaseModel.map{
-               dataMapper.mapEntityToModel(it)
-           }.toList()
+//       viewModel.readAllDataDB.observe(viewLifecycleOwner, Observer { listOfPodcastDatabaseModel ->
+//           binding.errorMessage.visibility =View.GONE
+//           binding.homeScreenProgressBar.visibility =View.GONE
+//           val listOfPodcasts = listOfPodcastDatabaseModel.map{
+//               dataMapper.mapEntityToModel(it)
+//           }.toList()
+//
+//           binding.homeFragmentRecyclerView.adapter = HomePodcastAdapter(listOfPodcasts)
+//
+//           val layoutManager = GridLayoutManager(requireContext(),2)
+//
+//           binding.homeFragmentRecyclerView.layoutManager = layoutManager
+//       })
 
-           binding.homeFragmentRecyclerView.adapter = HomePodcastAdapter(listOfPodcasts)
+        mainViewModel.readAllPodcast.observe(viewLifecycleOwner, Observer { listOfPodcast->
+            binding.errorMessage.visibility = View.GONE
+            binding.homeScreenProgressBar.visibility =View.GONE
 
-           val layoutManager = GridLayoutManager(requireContext(),2)
+            val podcastList = listOfPodcast.map { PodcastModel(
+                podcastId = it.podcastId,
+                title = it.title,
+                description = it.description,
+                language = it.language,
+                link = it.link,
+                imageUrl = it.imageUrl,
+                )
+            }.toList()
 
-           binding.homeFragmentRecyclerView.layoutManager = layoutManager
-       })
+            binding.homeFragmentRecyclerView.adapter = HomePodcastAdapter(podcastList){
+                it.podcastId?.let { mainViewModel.getPodcastWithId(it)}
+                mainViewModel.podcastsWithEpisodes?.observe(viewLifecycleOwner, Observer { listPodcastWithEpisode->
+                    val podcastsWithEpisodes =  listPodcastWithEpisode.elementAt(0)
+
+                    val podcastDetailWithEpisode = RssFeedResponse(
+                        title = podcastsWithEpisodes.podcast.title,
+                        description = podcastsWithEpisodes.podcast.description,
+                        language = podcastsWithEpisodes.podcast.language,
+                        link = podcastsWithEpisodes.podcast.link,
+                        imageUrl = podcastsWithEpisodes.podcast.imageUrl,
+                        episodes = podcastsWithEpisodes.episodes.map { dbPodcastEpisode ->
+                            RssFeedResponse.EpisodeResponse(
+                                title = dbPodcastEpisode.title,
+                                link = dbPodcastEpisode.link,
+                                description = dbPodcastEpisode.description,
+                                pubDate = dbPodcastEpisode.pubDate,
+                                duration = dbPodcastEpisode.duration,
+                                episodeUrl = dbPodcastEpisode.episodeUrl,
+                                imageUrl = dbPodcastEpisode.imageUrl,
+                                podcastName = dbPodcastEpisode.podcastName
+                            )
+                        }.toMutableList()
+                    )
+                    val action = HomeScreenFragmentDirections.actionHomeFragmentToPodcastDetailAndEpisodeFragment("")
+                        binding.root.findNavController().navigate(action)
+
+                    Log.d(TAG, "onViewCreated: $podcastDetailWithEpisode")
+
+                })
+            }
+
+            val layoutManager = GridLayoutManager(requireContext(),2)
+
+            binding.homeFragmentRecyclerView.layoutManager = layoutManager
+        })
     }
 
 
