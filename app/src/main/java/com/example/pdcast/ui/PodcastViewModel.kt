f5423bebdce753c601627b7e067b4ac5a96ae930
VIkash <itsmevikash09@gmail.com>
2021-10-15 01:46:52 +0530

    podcast viewModel

--- app/src/main/java/com/example/pdcast/ui/PodcastViewModel.kt
+++ app/src/main/java/com/example/pdcast/ui/PodcastViewModel.kt
@@ -1,23 +1,11 @@
 package com.example.pdcast.ui
 
-import android.content.Context
-import android.media.MediaMetadata
-import android.media.MediaMetadata.METADATA_KEY_ALBUM_ART_URI
-import android.net.Uri
-import android.os.Bundle
-import android.provider.Settings.Secure.getString
-import android.support.v4.media.MediaMetadataCompat
-import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI
-import android.support.v4.media.session.MediaControllerCompat
-import android.util.Log
-import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.pdcast.R
 import com.example.pdcast.data.api.RssFeedService
-import com.example.pdcast.data.model.PodcastModel
+import com.example.pdcast.data.database.PodcastSubscribedDatabase
+import com.example.pdcast.data.repository.RssFeedPodcastRepository
 import com.example.pdcast.data.response.RssFeedResponse
-import com.example.pdcast.ui.view.PodcastDetailAndEpisodeFragment
 import com.example.pdcast.util.Resource
 import com.example.pdcast.util.RssXmlParser
 import kotlinx.coroutines.CoroutineExceptionHandler
@@ -25,7 +13,6 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import java.lang.Exception
-import java.time.Duration
 
 class PodcastViewModel : ViewModel() {
 
@@ -35,10 +22,10 @@ class PodcastViewModel : ViewModel() {
     val podcast: SharedFlow<Resource<RssFeedResponse>> = _podcast
 
 
-
     private val rssXmlParser = RssXmlParser()
 
     private var isFirstTime: Boolean = true
+
 
 
 
