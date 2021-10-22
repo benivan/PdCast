7389f733f9533a722b544a9cd22c5fa487198fd3
VIkash <itsmevikash09@gmail.com>
2021-10-22 12:49:04 +0530

    some changes in query and insert function

--- app/src/main/java/com/example/pdcast/data/dao/RssFeedPodcastDao.kt
+++ app/src/main/java/com/example/pdcast/data/dao/RssFeedPodcastDao.kt
@@ -5,24 +5,28 @@ import androidx.room.*
 import com.example.pdcast.data.dto.DBPodcastsEpisodes
 import com.example.pdcast.data.dto.DBRssFeedPodcast
 import com.example.pdcast.data.dto.relations.PodcastsWithEpisodes
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RssFeedPodcastDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPodcast(podcast: DBRssFeedPodcast):Long
+    suspend fun insertPodcast(podcast: DBRssFeedPodcast): Long
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertEpisodes(episode: List<DBPodcastsEpisodes>)
 
     @Query("SELECT * FROM podcast_table ORDER BY podcastId ASC")
-    fun getAllPodcasts(): LiveData<List<DBRssFeedPodcast>>
+    fun getAllPodcasts(): Flow<List<DBRssFeedPodcast>>
 
     @Transaction
     @Query("SELECT * FROM podcast_table WHERE podcastId =:podcastId")
-    fun getEpisodeWithPodcast(podcastId:Long):LiveData<List<PodcastsWithEpisodes>>
+    fun getEpisodeWithPodcast(podcastId: Long): Flow<List<PodcastsWithEpisodes>>
 
     @Transaction
     @Query("SELECT * FROM podcast_table WHERE title=:podcastTitle")
-    fun getEpisodeWithPodcast(podcastTitle:String):LiveData<List<PodcastsWithEpisodes>>
+    fun getEpisodeWithPodcast(podcastTitle: String): Flow<List<PodcastsWithEpisodes>>
+
+    @Query("SELECT podcastId from podcast_table Where podcastFeedUrl=:feedLink")
+    fun getPodcastIdWithFeedUrl(feedLink: String): Long?
 }
