ae45769b80feb7179afb60ae10e56c8b6e9648c2
VIkash <itsmevikash09@gmail.com>
2021-10-24 19:45:57 +0530

    podcast model for view

---
+++ app/src/main/java/com/example/pdcast/data/model/PodcastEpisodeModel.kt
@@ -0,0 +1,12 @@
+package com.example.pdcast.data.model
+
+data class PodcastEpisodeModel (
+    var title: String? = null,
+    var link: String? = null,
+    var description: String? = null,
+    var pubDate: String? = null,
+    var duration: String? = null,
+    var episodeUrl: String? = null,
+    var imageUrl: String? = null,
+    var podcastName:String?=null
+)
