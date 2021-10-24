7fbc8532c5812666dc8087c2e01efac99c48756d
VIkash <itsmevikash09@gmail.com>
2021-10-24 19:47:01 +0530

    val  feedPodcast added

--- app/src/main/java/com/example/pdcast/data/model/PodcastModel.kt
+++ app/src/main/java/com/example/pdcast/data/model/PodcastModel.kt
@@ -2,6 +2,7 @@ package com.example.pdcast.data.model
 
 data class PodcastModel(
     val podcastId:Long?,
+    val podcastFeedUrl:String,
     val title:String,
     val description:String,
     val language:String,
