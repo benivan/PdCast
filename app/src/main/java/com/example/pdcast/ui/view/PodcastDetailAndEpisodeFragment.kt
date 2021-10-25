d71b24920e39c1f3c1d133fb3a397b4d199e1e2c
VIkash <itsmevikash09@gmail.com>
2021-10-26 03:28:50 +0530

    things changed

--- app/src/main/java/com/example/pdcast/ui/view/PodcastDetailAndEpisodeFragment.kt
+++ app/src/main/java/com/example/pdcast/ui/view/PodcastDetailAndEpisodeFragment.kt
@@ -9,34 +9,30 @@ import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
-import com.example.pdcast.data.dto.DBPodcast
-import com.example.pdcast.data.dto.DBPodcastsEpisodes
 import com.example.pdcast.data.dto.DBRssFeedPodcast
+import com.example.pdcast.data.repository.RssFeedPodcastRepository
 import com.example.pdcast.data.response.RssFeedResponse
 import com.example.pdcast.databinding.FragmentPodcastDetailAndEpisodeBinding
 import com.example.pdcast.ui.MainViewModel
 import com.example.pdcast.ui.PodcastViewModel
 import com.example.pdcast.util.Resource
-import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
 
 class PodcastDetailAndEpisodeFragment : Fragment() {
-
-    private lateinit var mediaBrowser: MediaBrowserCompat
-//    private var mediaControllerCallback: MediaControllerCallback? = null
 
     private val viewModel: PodcastViewModel by viewModels()
 
@@ -46,28 +42,22 @@ class PodcastDetailAndEpisodeFragment : Fragment() {
 
     private val binding: FragmentPodcastDetailAndEpisodeBinding get() = _binding!!
 
-    lateinit var feedLink: String
+    private val args by navArgs<PodcastDetailAndEpisodeFragmentArgs>()
+
+    private lateinit var episodeAdapter: EpisodeItemAdapter
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            feedLink = it.getString("feedLink").toString()
-        }
-        if (feedLink == ""){
-            Log.d(TAG, "onCreate: YESSSSSSSSS!!!!!!!!!")
-        }else{
-            viewModel.fetchXmlFromfeedUrl(feedLink)
-        }
-
+        viewModel.setRssFeedPodcastRepository(mainViewModel.getRssFeedPodcastRepository(),args.feedLink)
     }
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         super.onCreateView(inflater, container, savedInstanceState)
         _binding = FragmentPodcastDetailAndEpisodeBinding.inflate(layoutInflater, container, false)
         return binding.root
@@ -76,63 +66,65 @@ class PodcastDetailAndEpisodeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val handler = CoroutineExceptionHandler { _, throwable ->
-            throwable.printStackTrace()
-        }
-
-        binding.errorTextView.visibility = View.GONE
         binding.progressBarEpisodePage.visibility = View.GONE
+        binding.errorTextView.visibility = View.GONE
+        binding.podcastSubscribeButton.visibility = View.GONE
+
+
+        episodeAdapter = EpisodeItemAdapter(emptyList(), object : EpisodeListener {
+            override fun onEpisodeClicked(episodeViewData: RssFeedResponse.EpisodeResponse) {
+                val controller =
+                    MediaControllerCompat.getMediaController(requireActivity())
+
+                val activity = requireActivity()
+                if (controller.playbackState != null) {
+                    if (controller.playbackState.state ==
+                        PlaybackStateCompat.STATE_PLAYING
+                    ) {
+//                      controller.transportControls.pause()
+                        startPlaying(episodeViewData, activity)
+                    } else if (controller.playbackState.state == PlaybackStateCompat.STATE_PAUSED) {
+                        startPlaying(episodeViewData, activity)
+                    }
+                } else {
+                    startPlaying(episodeViewData, activity)
+                }
+            }
+        })
+        binding.rvEpisode.adapter = episodeAdapter
+
 
         viewModel.podcast.flowWithLifecycle(lifecycle).onEach {
             when (it) {
                 is Resource.Failure -> {
+                    Log.d(TAG, "onViewCreated: failure")
                     binding.errorTextView.visibility = View.VISIBLE
                     binding.progressBarEpisodePage.visibility = View.GONE
                     binding.errorTextView.text = it.toString()
+                    binding.podcastSubscribeButton.visibility = View.GONE
                 }
                 is Resource.Loading -> {
+                    Log.d(TAG, "onViewCreated: loading")
                     binding.progressBarEpisodePage.visibility = View.VISIBLE
                     binding.errorTextView.visibility = View.GONE
+                    binding.podcastSubscribeButton.visibility = View.GONE
                 }
                 is Resource.Success -> {
+                    Log.d(TAG, "onViewCreated: success")
                     binding.progressBarEpisodePage.visibility = View.GONE
                     binding.errorTextView.visibility = View.GONE
                     binding.tvDescription.visibility = View.GONE
+                    binding.podcastSubscribeButton.visibility = View.VISIBLE
                     binding.tvDescription.text = it.data.description
                     binding.tvLanguage.text = it.data.language
                     binding.tvTitle.text = it.data.title
                     Glide.with(binding.imageView).load(it.data.imageUrl).into(binding.imageView)
                     setSubscribeListener(it.data)
-                    binding.rvEpisode.adapter =
-                        EpisodeItemAdapter(it.data.episodes, object : EpisodeListener {
-                            override fun onEpisodeClicked(episodeViewData: RssFeedResponse.EpisodeResponse) {
-                                val controller =
-                                    MediaControllerCompat.getMediaController(requireActivity())
-
-                                val activity = requireActivity()
-                                if (controller.playbackState != null) {
-                                    if (controller.playbackState.state ==
-                                        PlaybackStateCompat.STATE_PLAYING
-                                    ) {
-//                                        controller.transportControls.pause()
-                                        startPlaying(episodeViewData, activity)
-                                    } else if (controller.playbackState.state == PlaybackStateCompat.STATE_PAUSED) {
-                                        startPlaying(episodeViewData, activity)
-                                    }
-                                } else {
-                                    startPlaying(episodeViewData, activity)
-                                }
-                            }
-
-                            override fun onSelectedEpisode(episodeViewData: RssFeedResponse.EpisodeResponse) {
-                            }
-                        })
-                    binding.rvEpisode.layoutManager = LinearLayoutManager(requireContext())
-                }
-                is Resource.None -> {
-
+                    episodeAdapter.submitList(it.data.episodes)
                 }
             }
+        }.catch { e ->
+            e.printStackTrace()
         }.launchIn(lifecycleScope)
     }
 
@@ -141,6 +133,7 @@ class PodcastDetailAndEpisodeFragment : Fragment() {
             mainViewModel.addRssPodcast(
                 DBRssFeedPodcast(
                     title = data.title.toString(),
+                    podcastFeedUrl = args.feedLink,
                     description = data.description.toString(),
                     language = data.language.toString(),
                     link = data.link.toString(),
