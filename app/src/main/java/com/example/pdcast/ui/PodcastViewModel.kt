b06c1aac76e5bb0a12511784c02e4ece9be6405d
VIkash <itsmevikash09@gmail.com>
2021-10-26 03:29:32 +0530

    added new thing

--- app/src/main/java/com/example/pdcast/ui/PodcastViewModel.kt
+++ app/src/main/java/com/example/pdcast/ui/PodcastViewModel.kt
@@ -1,61 +1,75 @@
 package com.example.pdcast.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pdcast.data.api.RssFeedService
 import com.example.pdcast.data.database.PodcastSubscribedDatabase
+import com.example.pdcast.data.dto.DBPodcastsEpisodes
+import com.example.pdcast.data.model.PodcastEpisodeModel
+import com.example.pdcast.data.model.PodcastModel
 import com.example.pdcast.data.repository.RssFeedPodcastRepository
 import com.example.pdcast.data.response.RssFeedResponse
 import com.example.pdcast.util.Resource
 import com.example.pdcast.util.RssXmlParser
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import java.lang.Exception
+import kotlin.math.log
 
-class PodcastViewModel : ViewModel() {
+class PodcastViewModel() : ViewModel() {
 
 
     private val _podcast = MutableSharedFlow<Resource<RssFeedResponse>>(1)
-
     val podcast: SharedFlow<Resource<RssFeedResponse>> = _podcast
 
-
-    private val rssXmlParser = RssXmlParser()
-
-    private var isFirstTime: Boolean = true
-
-
-
-
-    fun fetchXmlFromfeedUrl(it: String) {
-
+    fun setRssFeedPodcastRepository(
+        rssFeedPodcastRepository: RssFeedPodcastRepository,
+        feedUrl: String
+    ) {
         val handler = CoroutineExceptionHandler { _, throwable ->
             throwable.printStackTrace()
+            _podcast.tryEmit(Resource.Failure(throwable))
         }
-
-        viewModelScope.launch(handler) {
-            if (isFirstTime) {
-                isFirstTime = false
-                try {
-                    _podcast.emit(Resource.Loading())
-                    val response = RssFeedService.getFeedXml(it)
-                    val parseXml = rssXmlParser.parseXml(response.byteInputStream())
-
-                    _podcast.emit(Resource.Success(parseXml))
-                } catch (e: Exception) {
-                    _podcast.emit(Resource.Failure(e))
+        viewModelScope.launch(handler + Dispatchers.IO) {
+            _podcast.emit(Resource.Loading())
+            try {
+                Log.d(TAG, "getPodcastWithFeedUrl: ")
+                rssFeedPodcastRepository.getPodcastFromFeed(feedUrl).collect {
+                    val podcast = it.first()
+                    _podcast.emit(Resource.Success(
+                        RssFeedResponse(
+                            title = podcast.podcast.title,
+                            description = podcast.podcast.description,
+                            language = podcast.podcast.language,
+                            link = podcast.podcast.link,
+                            imageUrl = podcast.podcast.imageUrl,
+                            episodes = podcast.episodes.map {
+                                RssFeedResponse.EpisodeResponse(
+                                    title = it.title,
+                                    link = it.link,
+                                    description = it.description,
+                                    pubDate = it.pubDate,
+                                    duration = it.duration,
+                                    episodeUrl = it.episodeUrl,
+                                    imageUrl = it.imageUrl,
+                                    podcastName = it.podcastName
+                                )
+                            }.toMutableList()
+                        )
+                    ))
                 }
 
+
+            } catch (e: Exception) {
+                _podcast.emit(Resource.Failure(e))
             }
+
         }
-
     }
-
-
-
-
 
 
     override fun onCleared() {
