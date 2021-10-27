a78047d0f11a0ef53e8b8272a5b7f65b572b6646
VIkash <itsmevikash09@gmail.com>
2021-10-27 13:33:31 +0530

    created viewModel for playerFragment

---
+++ app/src/main/java/com/example/pdcast/ui/view/PlayerViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.pdcast.ui.view
+
+
+import android.support.v4.media.session.MediaControllerCompat
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pdcast.ui.MainViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class PlayerViewModel:ViewModel() {
+
+
+
+
+
+    companion object{
+        private const val TAG = "PlayerViewModel"
+    }
+
+}
+
