abf8e4d1d3f700ee5958f21cd2ff5799862b491c
VIkash <itsmevikash09@gmail.com>
2021-11-08 11:35:56 +0530

    gridLayout spanCount changed on the basic of orientation

--- app/src/main/java/com/example/pdcast/ui/view/HomeScreenFragment.kt
+++ app/src/main/java/com/example/pdcast/ui/view/HomeScreenFragment.kt
@@ -1,21 +1,26 @@
 package com.example.pdcast.ui.view
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.os.Parcelable
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pdcast.data.model.PodcastModel
 import com.example.pdcast.data.response.RssFeedResponse
 import com.example.pdcast.databinding.FragmentHomeScreenBinding
+import com.example.pdcast.ui.HomeViewModel
 import com.example.pdcast.ui.MainViewModel
 import com.example.pdcast.util.DataMapper
 import com.google.android.material.transition.MaterialFadeThrough
@@ -29,7 +34,7 @@ class HomeScreenFragment : Fragment() {
     private val binding: FragmentHomeScreenBinding get() = _binding!!
 
     private val mainViewModel: MainViewModel by activityViewModels()
-
+    private val viewModel: HomeViewModel by viewModels()
 
 
     override fun onCreateView(
@@ -51,34 +56,44 @@ class HomeScreenFragment : Fragment() {
         val dataMapper = DataMapper()
 
 
-        mainViewModel.readAllPodcast.onEach {  listOfPodcast->
+        mainViewModel.readAllPodcast.onEach { listOfPodcast ->
             binding.errorMessage.visibility = View.GONE
-            binding.homeScreenProgressBar.visibility =View.GONE
+            binding.homeScreenProgressBar.visibility = View.GONE
 
-            val podcastList = listOfPodcast.map { PodcastModel(
-                podcastId = it.podcastId,
-                podcastFeedUrl = it.podcastFeedUrl,
-                title = it.title,
-                description = it.description,
-                language = it.language,
-                link = it.link,
-                imageUrl = it.imageUrl,
+            val podcastList = listOfPodcast.map {
+                PodcastModel(
+                    podcastId = it.podcastId,
+                    podcastFeedUrl = it.podcastFeedUrl,
+                    title = it.title,
+                    description = it.description,
+                    language = it.language,
+                    link = it.link,
+                    imageUrl = it.imageUrl,
                 )
             }.toList()
 
-            binding.homeFragmentRecyclerView.adapter = HomePodcastAdapter(podcastList)
+            val adapter = HomePodcastAdapter(podcastList)
 
-            val layoutManager = GridLayoutManager(requireContext(),2)
+            val orientation = this.resources.configuration.orientation;
+            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                val layoutManager = GridLayoutManager(requireContext(), 2)
+                binding.homeFragmentRecyclerView.layoutManager = layoutManager
+                binding.homeFragmentRecyclerView.adapter = adapter
+            } else {
+                val layoutManager = GridLayoutManager(requireContext(), 5)
+                binding.homeFragmentRecyclerView.layoutManager = layoutManager
+                binding.homeFragmentRecyclerView.adapter = adapter
+            }
 
-            binding.homeFragmentRecyclerView.layoutManager = layoutManager
-        }.launchIn(lifecycleScope)
+
+
+            }.launchIn(lifecycleScope)
+        }
+
+
+        companion object {
+            private const val TAG = "HomeScreen"
+        }
+
+
     }
-
-
-
-    companion object {
-        private const val TAG = "HomeScreen"
-    }
-
-
-}
