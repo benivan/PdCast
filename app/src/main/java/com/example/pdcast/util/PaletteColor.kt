1466d879cb1f6b1ab73321770b04e16e6ea9f9f0
VIkash <itsmevikash09@gmail.com>
2021-11-12 17:47:32 +0530

    color palette type

---
+++ app/src/main/java/com/example/pdcast/util/PaletteColor.kt
@@ -0,0 +1,10 @@
+package com.example.pdcast.util
+
+data class PaletteColor(
+    val vibrant:Int,
+    val vibrantLight:Int,
+    val vibrantDark: Int,
+    val muted :Int,
+    val mutedLight:Int,
+    val mutedDark:Int,
+)
