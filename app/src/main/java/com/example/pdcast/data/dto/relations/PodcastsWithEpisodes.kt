22ed37532031be1547a8e3dd829e7e66cef5f01a
VIkash <itsmevikash09@gmail.com>
2021-11-17 19:27:36 +0530

    some change

--- app/src/main/java/com/example/pdcast/data/dto/relations/PodcastsWithEpisodes.kt
+++ app/src/main/java/com/example/pdcast/data/dto/relations/PodcastsWithEpisodes.kt
@@ -4,6 +4,7 @@ import androidx.room.Embedded
 import androidx.room.Relation
 import com.example.pdcast.data.dto.DBPodcastsEpisodes
 import com.example.pdcast.data.dto.DBRssFeedPodcast
+import kotlinx.coroutines.flow.Flow
 
 data class PodcastsWithEpisodes(
     @Embedded
