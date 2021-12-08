e9ac13c09dc69fb1540f6ff04b6a75d7658d9385
VIkash <itsmevikash09@gmail.com>
2021-12-09 01:13:30 +0530

    make entity updatable

--- app/src/main/java/com/example/pdcast/data/dto/DBRssFeedPodcast.kt
+++ app/src/main/java/com/example/pdcast/data/dto/DBRssFeedPodcast.kt
@@ -3,10 +3,12 @@ package com.example.pdcast.data.dto
 import androidx.room.*
 
 
+@Update
 @Entity(tableName = "podcast_table")
 data class DBRssFeedPodcast(
     @PrimaryKey(autoGenerate = true)
     val podcastId: Long? = null,
+    val isSubscribe:Boolean,
     val podcastFeedUrl:String,
     val title: String,
     val description: String,
