ef150e473cdde699c34019d78af621f74a00f2c8
VIkash <itsmevikash09@gmail.com>
2021-12-10 14:23:20 +0530

    added the getPaletteColor

--- app/src/main/java/com/example/pdcast/util/PaletteColor.kt
+++ app/src/main/java/com/example/pdcast/util/PaletteColor.kt
@@ -1,5 +1,11 @@
 package com.example.pdcast.util
 
+import android.content.Context
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resumeWithException
+
 data class PaletteColor(
     val vibrant:Int,
     val vibrantLight:Int,
@@ -8,3 +14,28 @@ data class PaletteColor(
     val mutedLight:Int,
     val mutedDark:Int,
 )
+
+suspend fun getPaletteColor(uri: String?, context:Context) = suspendCancellableCoroutine<PaletteColor> {
+
+    try {
+        val theBitmap = Glide.
+        with(context).asBitmap().load(uri).submit().get()
+        Palette.from(theBitmap).generate { palette ->
+            val colorPalette = PaletteColor(
+                vibrant = palette!!.getVibrantColor(0x000000),
+                vibrantLight = palette.getLightVibrantColor(0x000000),
+                vibrantDark = palette.getDarkVibrantColor(0x000000),
+                muted = palette.getMutedColor(0x000000),
+                mutedLight = palette.getLightMutedColor(0x000000),
+                mutedDark = palette.getDarkMutedColor(0x000000)
+            )
+            it.resumeWith(Result.success(colorPalette))
+        }
+
+
+    } catch (e: Exception) {
+        it.resumeWithException(e)
+    }
+
+
+}
